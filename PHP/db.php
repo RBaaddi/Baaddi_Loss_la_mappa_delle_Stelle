2ac90a3924eecc25c83207f2ee9c08c3517cdcc5
Rayan <rayan.baaddi@buonarroti.tn.it>
2026-03-26 20:41:32 +0100

    modifica aggiuntiva per finire il progetto

--- PHP/db.php
+++ PHP/db.php
@@ -1,5 +1,3 @@
-//Il nome del database è: db_la_mappa_delle_stelle
-
 <?php
 $conn = new mysqli("localhost", "root", "", "db_la_mappa_delle_stelle");
 if ($conn->connect_error) die("Errore: " . $conn->connect_error);
